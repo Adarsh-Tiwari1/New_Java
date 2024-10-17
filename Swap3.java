12afd0f8a03c00eda5aebbea188d51ca4be211b1
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-17 10:15:13 +0530

    Swap number

---
+++ Swap3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Swap3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        a = a+b; 
+        b = a-b; 
+        a = a-b;
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+    }
+}
