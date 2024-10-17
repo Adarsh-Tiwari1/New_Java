ba8184e963ca87f89619fc6058614233e869fa90
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-18 00:53:11 +0530

    PreDecrenment in Java

---
+++ PreDec.java
@@ -0,0 +1,16 @@
+ 
+class PreDec{
+    public static void main(String[] args){
+        int a, b;
+        a = 10;
+        b = 20;
+        a++;
+        b++;
+        ++a;
+        ++b;
+        a++;
+        b++;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
