24b3efa502390c44757003b81b32cbc52b741b93
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-18 00:50:27 +0530

    Student information

---
+++ StudentData.java
@@ -0,0 +1,51 @@
+
+class StudentData{
+	public static void main(String[] args){
+	// Declaration 
+	String name;
+	String enroll;
+	char s;
+	int m;
+	int p;
+	int c;
+	int h;
+	int e;
+	int total;
+	float per;
+
+	// initialization
+	name="adarsh";
+	enroll="0191ca231005";
+	s='A';
+	m=88;
+	p=67;
+	c=78;
+	h=66;
+	e=56;
+	total=m+p+c+h+e;
+	per=total/5.0f;
+
+	// Print value of the variable
+	System.out.println("Name : " + name);
+	System.out.println("Enrollment No : " + enroll);
+	System.out.println("Section : " + s);
+	System.out.println("Math : " + m);
+	System.out.println("Physics : " + p);
+	System.out.println("Chemistry : " + c);
+	System.out.println("Hindi : " + h);
+	System.out.println(" English : " + e);
+	System.out.println("Total Marks :" + total);
+	System.out.println("Per : " + per);
+	}
+}
+
+
+
+
+
+
+
+
+
+	
+	
