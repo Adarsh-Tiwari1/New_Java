f79e295de595121ce9bb33e3a7baff3a952c481b
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-18 00:51:54 +0530

    How to print in java program

---
+++ PrintMethod.java
@@ -0,0 +1,17 @@
+
+public class PrintMethod{
+    public static void main(String[] args){
+        System.out.print("Adarsh \n");
+        System.out.print("Kumar \n");
+        System.out.print("Tiwari");
+
+        System.out.println(" ");
+
+        System.out.println("Adarsh");
+        System.out.println("Kumar");
+        System.out.println("Tiwari");
+
+        System.out.printf("%s", "Adarsh \n");
+        System.out.printf("%s", "Tiwari");
+    }
+}
