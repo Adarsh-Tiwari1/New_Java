55c00d10c9ef46c85a97df304de5953f90ebaa39
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-17 10:27:58 +0530

    Sum two number

---
+++ Sum1.java
@@ -0,0 +1,9 @@
+
+import javax.swing.*;
+class Sum1{
+    public static void main(String[] args){
+        int a = Integer.parseInt(JOptionPane.showInputDialog("Enter 1st number"));
+        int b = Integer.parseInt(JOptionPane.showInputDialog("Enter 2nd number"));
+        System.out.println(a+b);
+    }
+}
