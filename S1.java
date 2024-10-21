010f857039f414bf574c2a8c3733039f41623d01
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-21 18:33:43 +0530

    I Trying to String

---
+++ S1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class S1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println("Sum of two number : " + (num1 + num2));
+    }
+}
