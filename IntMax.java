ef20485d217d87886061f20c94cce373863cdff3
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-21 18:32:37 +0530

    int max value

---
+++ IntMax.java
@@ -0,0 +1,11 @@
+
+class IntMax{
+    public static void main(String[] args){
+        System.out.println("int max value : "  + Integer.MAX_VALUE);
+        System.out.println( "int min value : "  + Integer.MIN_VALUE);
+        System.out.println( "float max value : "  +Float.MAX_VALUE);
+        System.out.println( "int min value : "  + Float.MIN_VALUE);
+        System.out.println( " char min value : "  + Character.MIN_VALUE);
+        System.out.println( "char max value : "  + Character.MAX_VALUE);
+    }
+}
