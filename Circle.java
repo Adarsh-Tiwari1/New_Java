056f96ad12a44faea1dd34cf1458166f7a92612a
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-21 23:32:44 +0530

    How To Find Circle

---
+++ Circle.java
@@ -0,0 +1,11 @@
+class Circle{
+	public static void main(String[] args){
+	float r,ar,cr;
+	r=2.0f;
+	ar=3.14f*r*r;
+	cr=2*3.14f*r;
+	System.out.println("radius is :"+ r);
+	System.out.println("area f circle :"+ar);
+	System.out.println("circumference of circle :"+cr);
+	}
+}
