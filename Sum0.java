fc4a3f1a39ee1e4223cbac529fbc64ebee414c76
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-21 23:31:44 +0530

    Sum Two Number

---
+++ Sum0.java
@@ -0,0 +1,20 @@
+class Sum0{
+	public static void main(String[] args){
+	// declare the variable
+	int a;
+	int b;
+	int c;
+	// initialize the variable
+	a=10;
+	b=20;
+	// perform the operation
+	c=a+b;
+	// print value of the variable
+	System.out.println("Number1 :"+ a);
+	System.out.println("Number2 :"+b);
+	System.out.println("Number3 :"+c);
+	}
+}
+	
+	
+	
