bc59f122ea80d9fcdfd93e464a3e9201056e9648
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-21 23:30:57 +0530

    How to feed Student name and Roll No

---
+++ StudentData0.java
@@ -0,0 +1,20 @@
+class StudentData0{
+	public static void main(String[] args){
+	// declare 
+	String name;
+	char s;
+	float per;
+	int rno;
+
+
+	// initialize 
+	name = "adarsh";
+	s='a';
+	rno=101;
+
+	// print value of the variable
+	System.out.println("roll no :"+ rno);
+	System.out.println("name :"+name);
+	System.out.println("section :"+s);
+	}
+}
