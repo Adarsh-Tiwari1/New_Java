9643d769d84be0b3b7c85063a667437e5eaa6cfd
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-22 20:37:53 +0530

    Scanner Class

---
+++ Sca.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Sca{
+	public static void main(String[] args){
+	int n1,n2,n3;
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter number 1 : ");
+	n1=sc.nextInt();
+	System.out.println("Enter number 2 : ");
+	n2=sc.nextInt();
+	n3=n1+n2;
+	System.out.println("Result : " + n3);
+	}
+}
+
+	
