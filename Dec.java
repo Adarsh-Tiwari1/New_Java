cb6973bed50f8b92edc9114538d396c33e923dc4
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-23 22:13:56 +0530

    Some

---
+++ Dec.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+	class Dec{
+	public static void main(String[] args){
+	int a,b;
+	a=10;
+	b=20;
+	--a;
+	b--;
+	System.out.println(a);
+	System.out.println(b);
+	}
+}
+
+	
