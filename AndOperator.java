5b3dfd8b781c6f40c973d0cf78c116fee1611b86
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-24 21:01:55 +0530

    And Operator

---
+++ AndOperator.java
@@ -0,0 +1,12 @@
+
+class AndOperator{
+    public static void main(String[] args){
+        int a,b,c,d; // one false so all result false 
+        a=10;
+        b=20;
+        c=30;
+        d=40;
+        boolean r=a<b && c>d;
+        System.out.println(r);
+    }
+}
