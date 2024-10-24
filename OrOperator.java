e0339ec2bd46a8b6d67290f8dac14a97d95cbf62
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-24 20:59:25 +0530

    Or Operator

---
+++ OrOperator.java
@@ -0,0 +1,13 @@
+
+
+class OrOperator{
+    public static void main(String[] args){
+        int a,b,c,d; // one false so all result false 
+        a=10;
+        b=20;
+        c=30;
+        d=40;
+        boolean r=a<b || c>d;
+        System.out.println(r);
+    }
+}
