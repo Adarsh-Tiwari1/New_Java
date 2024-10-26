82ec39812108f769ca3403f2c066f6be3f54ac1d
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-26 21:32:45 +0530

    Area Of Square By Scanner Class

---
+++ AreaOfSquare.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class AreaOfSquare {
+    public static void main(String[] args) {
+        
+    
+    int side ,area;
+    System.out.println("enter side square");
+    Scanner r=new Scanner(System.in);
+    side=r.nextInt();
+    area=side*side;
+     System.out.println("area of square "+area);
+    
+    }
+}
