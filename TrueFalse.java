0de2c40436b03c4168518be32c34e5ed1443102a
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-28 15:07:15 +0530

    True and False

---
+++ TrueFalse.java
@@ -0,0 +1,17 @@
+
+public class TrueFalse {
+    public static void main(String[] args) {
+        boolean a = (true || false);
+        boolean b = (true && false);
+        boolean c = (true != false);
+        boolean d = (true == false);
+        boolean e = true;
+        boolean f = false;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
