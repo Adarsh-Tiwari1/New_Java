830dfd6dd9265c422a8e3cf45216341ee423e9b4
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-29 19:51:57 +0530

    boolean method

---
+++ Bool.java
@@ -0,0 +1,12 @@
+
+class Bool{
+    public static void main(String[] args){
+        boolean a =  true;
+       // boolean A = boolean TrueFalse; // error
+       // boolean a1 = boolean TRUE; // // error
+        
+        System.out.println(a);
+       // System.out.println(A);
+       // System.out.println(a1);
+    }
+}
