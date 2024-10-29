448311b7fcadd99f4b094d039efbbd9157d39b78
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-29 19:54:47 +0530

    Logical And Operator

---
+++ LogicOpe.java
@@ -0,0 +1,11 @@
+
+class LogicOpe{
+    public static void main(String[] args){
+        int num = 0 | 1;
+        int num1 = 0 & 1;
+       // int num2 = 0 != 1;
+        System.out.println(num);
+        System.out.println(num1);
+       // System.out.println(num2);
+    }
+}
