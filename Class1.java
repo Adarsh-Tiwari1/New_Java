bc66a841594d15431e33ece76b4b6505020b8491
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-30 22:19:13 +0530

    Some code

---
+++ Class1.java
@@ -0,0 +1,11 @@
+
+class Class1{
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 5;
+        if(n1 > n2)
+            System.out.println(n1);
+            System.out.println(n2);
+
+    }
+}
