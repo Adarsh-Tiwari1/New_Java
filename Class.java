782c253acf1ea54eb6f50b6feb84d165a3b11305
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-30 22:18:09 +0530

    How to use class Name Class

---
+++ Class.java
@@ -0,0 +1,11 @@
+
+class Class{
+    public static void main(String[] args){
+        int age = 10;
+        if(age == 10){
+            System.out.println("xxx");
+        }else{
+            System.out.println("yyy");
+        }
+    }
+}
