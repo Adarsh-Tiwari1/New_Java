c96112474488bd31ee47d0720daae74561fed024
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-31 22:33:20 +0530

    Some Code

---
+++ Code1.java
@@ -0,0 +1,9 @@
+
+class Code1{
+    public static void main(String[] args){
+        String name = "Adarsh Tiwari";
+        Object obj = 123;
+        System.out.println(name);
+        System.out.println(obj);
+    }
+}
