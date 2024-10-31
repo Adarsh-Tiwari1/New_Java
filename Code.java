ccd7d15971a8246b965a60074778852259040cdb
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-10-31 22:32:25 +0530

    Some Try Question

---
+++ Code.java
@@ -0,0 +1,13 @@
+
+class Code{
+    public static void main(String[] args){
+        int num = 1000000;
+        float num1 = 10000000.0f;
+        float num3 = 10000f;
+        double num2 = 1000.0;
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+    }
+}
