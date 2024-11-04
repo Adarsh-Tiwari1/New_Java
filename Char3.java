ac6681929fc035cc24720bebccc29b3a28330529
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-04 10:25:34 +0530

    Character

---
+++ Char3.java
@@ -0,0 +1,10 @@
+
+class Char3{
+    public static void main(String[] args){
+        System.out.println('a');
+        System.out.println('a'+'a');
+        System.out.println(""+'a');
+        System.out.println('a'+"a");
+        System.out.println('a'+"");
+    }
+}
