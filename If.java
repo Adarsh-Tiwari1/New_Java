acde0628dba32a448715f713d2d2d69e0b53cc0a
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-06 22:13:55 +0530

    Coding

---
+++ If.java
@@ -0,0 +1,126 @@
+/*
+import java.util.Scanner;
+
+class If{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Number");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter Second Number");
+        int num2 = sc.nextInt();
+
+        if(num1 > num2)
+        System.out.println("Number 1st is Greater");
+        System.out.println("This is 1st Statenment");
+
+        if(num1 < num2)
+        System.out.println("Number 1st is Greater");
+        System.out.println("This is 2st Statenment");
+
+        if(num1 == num2)
+        System.out.println("Number 1st is Greater");
+        System.out.println("This is 3st Statenment");
+    }
+}
+*/
+
+
+/*
+import java.util.Scanner;
+
+class If{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Number");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter Second Number");
+        int num2 = sc.nextInt();
+
+        if(num1 > num2) {
+        System.out.println("Number 1st is Greater");
+        System.out.println("This is 1st Statenment");
+        } 
+        if(num1 < num2) {
+        System.out.println("Number 1st is Greater");
+        System.out.println("This is 2st Statenment");
+        }
+        if(num1 == num2) {
+        System.out.println("Number 1st is Greater");
+        System.out.println("This is 3st Statenment");
+        }
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class If{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First Number");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter Second Number");
+        int num2 = sc.nextInt();
+
+        if(num1 > num2) {
+        System.out.println("Number 1st is Greater");
+        System.out.println("This is 1st Statenment");
+        } 
+        if(num1 < num2) {
+        System.out.println("Number 2st is Greater");
+        System.out.println("This is 2st Statenment");
+        }
+        if(num1 == num2) {
+        System.out.println("Number 3st is Greater");
+        System.out.println("This is 3st Statenment");
+        }
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class If{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter  Number");
+        int num1 = sc.nextInt();
+
+        if(num1 > 0) {
+        System.out.println("This is Positive Number");
+        } 
+        if(num1 < 0) {
+        System.out.println(" This is Negative Nmber");
+        }
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class If{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter  Number");
+        int num1 = sc.nextInt();
+
+        if(num1 % 2 == 0) {
+        System.out.println("This is Even Number");
+        } 
+        if(num1 % 2 != 0) {
+        System.out.println(" This is Odd Nmber");
+        }
+    }
+}
+*/
