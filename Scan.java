d075fd1567aada41d78ee1d105a304b0b16afedc
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-07 23:26:17 +0530

    I Trying To Scanner Class

---
+++ Scan.java
@@ -0,0 +1,41 @@
+/*
+class Scan{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
+*/
+
+
+/*
+import java.util.Scanner;
+              // Scanner is already defined in this compilation unit
+class Scan{  // Scanner Naam Ke Class Hum Nahi Bna Sakte Hai.
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
+*/
+
+
+/*
+class Scan{
+    public static void main(String[] args){
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
+*/
+
