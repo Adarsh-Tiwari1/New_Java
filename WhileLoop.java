9ede7b4170c68ded0471f8c67b33e1fa49456ace
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-11 19:41:12 +0530

    I Tryint to whiel loop

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+
+class WhileLoop{
+    public static void main(String[] args){
+        int a;
+        a = 1; // initialization
+
+        while(a <= 5){ // test consition
+        System.out.println(a);
+        a++; // increment 
+        }
+    }
+}
