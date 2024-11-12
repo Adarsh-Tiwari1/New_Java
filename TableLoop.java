7700aa249de2ee26c93633c975c65e0cfe50952e
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-12 20:16:05 +0530

    Print Table Using for, while, and do-while look

---
+++ TableLoop.java
@@ -0,0 +1,76 @@
+/*
+import java.util.Scanner;
+
+class TableLoop{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num,i;
+        System.out.println("Enter Any Number");
+        num = sc.nextInt();
+        i=1;
+        while(i<=10){
+            System.out.println(num + " * " + i + " = " + (num * i));
+            i++;
+        }
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class TableLoop{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number");
+        int num = sc.nextInt();
+        int i = 1;
+        do{
+            System.out.println(num + " * " + i + " = " + (num * i));
+            i++;
+        }while(i <= 10);
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class TableLoop{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Number");
+        int num = sc.nextInt();
+
+        for()
+    }
+}
+*/
+
+
+
+// wap to print multiple of 3 and 5 from 1 to 60 using for, while, do-while
+
+
+/*
+class TableLoop{
+    public static void main(String[] args){
+        for(int i=1;i<=60;i++){
+            if(i%3==0 && i%5==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+*/
+
+
+class TableLoop{
+    public static void main(String[] args){
+        int num,i;
+        while()
+    }
+}
