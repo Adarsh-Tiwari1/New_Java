6caba913f1336422a480e51d4c28732ad82b20b2
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-12 20:17:20 +0530

    I Trying to for loop

---
+++ ForLoop.java
@@ -0,0 +1,63 @@
+/*
+class ForLoop{
+    public static void main(String[] args){
+        int i;
+        for(i=1;i<=5;i++){
+            System.out.println(i);
+        }
+    }
+}
+*/
+
+/*
+class ForLoop{
+    public static void main(String[] args){
+        int i;
+        i=10;
+        while(i<=20){
+            if(i%2==0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
+*/
+
+/*
+class ForLoop{
+    public static void main(String[] args){
+        int i;
+        i=10;
+        do{
+            if(i%2==0){
+                System.out.println("Even : " + i);
+            }
+            i++; 
+        }while(i<=20);
+    }
+}
+*/
+
+/*
+class ForLoop{
+    public static void main(String[] args){
+        for(int i=10;i<=20;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
+*/
+
+/*
+class ForLoop{
+    public static void main(String[] args){
+        for(int i=10;i<=20;i=i+2){    
+            System.out.println(i);   
+        }
+    }
+}
+*/
+
