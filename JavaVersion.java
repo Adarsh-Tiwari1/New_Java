3e7ae285e8d3e65d83adc75915f7cae0ebaa716d
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-13 20:20:23 +0530

    Java Version

---
+++ JavaVersion.java
@@ -0,0 +1,19 @@
+/*
+class JavaVersion {    
+public static void main(String args[]) {    
+
+String str=System.getProperty("java.version");  
+//prints Java version  
+System.out.println("Java Version is: "+str);    
+}    
+}   
+*/
+
+
+class JavaVersion{
+    public static void main(String args[]){
+
+        String str = System.getProperty("java.version");
+        System.out.println("Java version is : " + str);
+    }
+}
