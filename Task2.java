d521c2587c97f91e3f48a3d0de109864f9bdc6e9
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-15 22:28:38 +0530

    Task Question

---
+++ Task2.java
@@ -0,0 +1,78 @@
+/*
+class Task2{
+    public static void main(String[] args){
+        String Hello = "Hello";
+        String Name = "Adarsh Kumar Tiwari";
+        System.out.println(Hello);
+        System.out.println(Name);
+    }
+}
+*/
+
+/*
+class Task2{
+    public static void main(String[] args){
+        int num1 = 74;
+        int num2 = 36;
+        System.out.println(num1 + num2);
+    }
+}
+*/
+
+
+/*
+class Task2{
+    public static void main(String[] args){
+        int num1 = 50;
+        int num2 = 3;
+        int result = num1 / num2;
+        System.out.println(result);
+    }
+}
+*/
+
+/*
+class Task2{
+    public static void main(String[] args){
+        int result1 = -5 + 8 * 6;
+        int result2 = (55 + 9) % 9;
+        int result3 = 20 + -3 * 5 / 8;
+        int result4 = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+    }
+}
+*/
+
+/*
+class Task2{
+    public static void main(String[] args){
+        int num1 = 25;
+        int num2 = 5;
+        int result = num1 * num2;
+        System.out.println(result);
+    }
+}
+*/
+
+/*
+class Task2{
+    public static void main(String[] args){
+        int num1 = 125;
+        int num2 = 24;
+        int Addition = num1 + num2;
+        int Subtract = num1 - num2;
+        int Multiply = num1 * num2;
+        int Divide = num1 / num2;
+        int Modulo = num1 % num2;
+
+        System.out.println(Addition);
+        System.out.println(Subtract);
+        System.out.println(Multiply);
+        System.out.println(Divide);
+        System.out.println(Modulo);
+    }
+}
+*/
