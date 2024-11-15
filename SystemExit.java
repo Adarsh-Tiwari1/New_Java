747b30f7e5c309925a9ff8ce7111f89a29e6f090
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-15 22:29:32 +0530

    systemExit(0)

---
+++ SystemExit.java
@@ -0,0 +1,10 @@
+
+
+class SystemExit{
+    public static void main(String[] args){
+        System.out.println("Hello");
+       // System.exit('a');
+        System.exit(10);
+        System.out.println("Adarsh");
+    }
+}
