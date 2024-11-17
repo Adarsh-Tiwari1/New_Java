be32fb41ca6cd7a281005490425c151837b18c57
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-17 10:15:40 +0530

    Number Pattern Question

---
+++ NumberPattern.java
@@ -0,0 +1,11 @@
+
+class NumberPattern{
+    public static void main(String[] args){
+    for(int r=1;r<=5;r++){
+        for(int c=1;c<=r;c++){
+            System.out.print(r + " "); 
+        }
+        System.out.println(""); 
+    }
+    }
+}
