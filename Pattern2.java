0ea7fe926d9fb9b0b92b1f717f2270002eb333fb
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-17 10:12:28 +0530

    Pattern Based Question

---
+++ Pattern2.java
@@ -0,0 +1,12 @@
+
+
+class Pattern2{
+    public static void main(String[] args){
+    for(int r=5;r>=1;r--){
+        for(int j=1;j<=r;j++){
+            System.out.print("* "); 
+        }
+        System.out.println(""); 
+    }
+    }
+}
