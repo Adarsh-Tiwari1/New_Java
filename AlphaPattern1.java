c4a0e480fb20d598e69d3281edc134a1bcf33f32
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-19 22:57:20 +0530

    Character Based Pattern Question

---
+++ AlphaPattern1.java
@@ -0,0 +1,13 @@
+
+class AlphaPattern1{
+    public static void main(String[] args){
+        
+        for(int r=5;r<=1;r--){
+            for(int c=1;c<=r;c++){
+                
+                System.out.print((char)(64+r) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
