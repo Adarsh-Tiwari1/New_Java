4224e276550333ae9d00ffdb6331a26247c05f02
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-19 22:53:52 +0530

    Number Based Pattern

---
+++ NumPattern3.java
@@ -0,0 +1,12 @@
+
+
+class NumPattern3{
+    public static void main(String[] args){
+        for(int r=5;r>=1;r--){
+            for(int c=1;c<=r;c++){
+                System.out.print(c + " ");
+            }
+                System.out.println();
+        }
+    }
+}
