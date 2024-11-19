2e0f7602e2d31c7d278106705f3d946c5df59d31
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-19 23:00:40 +0530

    Pattern Question

---
+++ AdvPatern.java
@@ -0,0 +1,32 @@
+/*
+class AdvPatern{
+    public static void main(String[] args){
+        
+        for(int r=1;r<=5;r++){
+            for(int c=1;c<=r;c++){
+                if((r+c)%2==0){
+                    System.out.print("0 ");
+                }else{
+                    System.out.print("1 ");
+                }
+            }
+                System.out.println();
+        }
+    }
+}
+*/
+
+
+/*
+class AdvPatern{
+    public static void main(String[] args){
+        
+        for(int r=1;r<=5;r++){
+            for(int c=1;c<=r;c++){
+                System.out.print((r+c)%2==0);
+            }
+                System.out.println();
+        }
+    }
+}
+*/
