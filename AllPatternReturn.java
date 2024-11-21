cb92426f32c9ec2aae3ecaf66de381ada12de081
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-21 22:02:30 +0530

    Some Pattern Question

---
+++ AllPatternReturn.java
@@ -0,0 +1,272 @@
+// Right Triangle Star Pattern
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=1;r<=5;r++){
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// Left Triangle Star Pattern 
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=1;r<=5;r++){
+            for(int s=5;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// Pyramid Star Pattern
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=1;r<=5;r++){
+            for(int s=5;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+//  Diamond Shape Pattern
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=1;r<=5;r++){
+            for(int s=5;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        for(int r=4;r>=1;r--){
+            for(int s=4;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// Downward Triangle Star Pattern
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=7;r>=1;r--){
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            for(int s=7;s>r;s--){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+}    
+*/
+
+// Mirrored Right Triangle Star Pattern
+
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=1;r<=8;r++){
+            for(int s=8;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+//  Reverse Pyramid Star Pattern
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=8;r>=1;r--){
+            for(int s=8;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// Right Down Mirror Star Pattern
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=7;r>=1;r--){
+            for(int s=7;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// ight Pascal's Triangle
+
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=1;r<=8;r++){
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            for(int s=8;s>r;s--){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        
+        for(int r=7;r>=1;r--){
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            for(int s=8;s>r;s--){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// Left Pascal's Triangle
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=1;r<=10;r++){
+            for(int s=10;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+        for(int r=9;r>=1;r--){
+            for(int s=10;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// Sandglass Star Pattern
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=10;r>=1;r--){
+            for(int s=10;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+
+        for(int r=1;r<=10;r++){
+            for(int s=10;s>r;s--){
+                System.out.print(" ");
+            }
+            for(int c=1;c<=r;c++){
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// Alphabet Star Pattern : 12
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=5;r>=1;r--){
+            if()
+        }
+    }
+}
+*/
+
+// Triangle Star Pattern : 13 , 14 , 15
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=5;r>=1;r--){
+            
+        }
+    }
+}
+*/
+
+
+// Number Pattern : Pattern-1
+/*
+class AllPatternReturn{
+    public static void main(String[] args){
+        for(int r=1;r<=7;r++){
+            for(int c=1;c<=r;c++){
+                System.out.print(c + " ");
+            }
+            for(int s=7;s>r;s--){
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+}
+*/
+
+// Pattern 4
+
+class AllPatternReturn{
+    public static void main(String[] args){
+        for()
+    }
+}
