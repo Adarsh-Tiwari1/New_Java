3e881125ac5ab0e76c0b9f6d0c270439adf3abc1
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-22 18:36:18 +0530

    Add Num

--- Add.java
+++ Add.java
@@ -1,8 +1,10 @@
+/*
 import java.util.Scanner;
 
 class Add{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter number");
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
@@ -11,5 +13,18 @@ class Add{
         System.out.println("Multi tow number : " + (num1 * num2));
         System.out.println("Dividt two number : " + (num1 / num2));
         System.out.println("Modulto tow number : " + (num1 % num2));
+    }
+}
+*/
+
+
+public class Add{
+   public  static void m1(){
+        System.out.print("Pack New _ java");
+    }
+    public static void main(String[] args){
+        int num;
+        num = 0;
+        System.out.println(num);
     }
 }
