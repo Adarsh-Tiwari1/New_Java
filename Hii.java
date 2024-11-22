22350a5815c8fba5cf2fe26466eaf53753208d68
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-22 18:38:07 +0530

    Some Code

--- Hii.java
+++ Hii.java
@@ -1,28 +1,177 @@
+/*
+import java.util.Scanner;
 
+class Hii {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        int num = sc.nextInt();
+        
+        while (num > 0) {
+            int rem = num % 10; // Get the last digit
+            if (rem == 3) {     // Check if the digit is 3
+                count++;
+                break;          // Exit the loop if 3 is found
+            }
+            num = num / 10;     // Remove the last digit
+        }
+        
+        if (count != 0) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
+*/
+
+/*
+class Hii{
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 20;
+        System.out.print(num1);
+        System.out.print(" ");
+        System.out.print(num2);
+        System.out.print(" ");
+        System.out.print(num1 + num2);
+    }
+}
+*/
+
+/*
+class Hii{
+    public static void main(String[] args){
+        System.out.printf("Hello Java \\n");
+        System.out.printf("Hello Adarsh");
+    }
+}
+*/
+
+/*
+class Hii{
+    public static void main(String[] args){
+        int num = 0;
+        int num2 = 00;
+        
+        System.out.printf("%d", num);
+        System.out.printf("\n%d", num2);
+        
+    }
+}
+*/
+
+/*
+class Hii{
+    public static void main(String[] args){
+        int num = 10;
+        int num2 = 100;
+        
+        System.out.println(num+num2/2f);
+    }
+}
+*/
+
+
+/*
 import java.util.Scanner;
 
 class Hii{
     public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-        int count=0;
-        int num = sc.nextInt();
-        while(num>0)
-        {
-            int rem=num%10;
-            num=num/10;
-            if(rem==3 || num==3)
-            {
-                count++;
-                break;
-            }
+        Scanner sc = new Scanner(System.in);
 
+        String fullName = sc.nextLine();
+        String name = sc.next();
+        char ch = sc.next().charAt(0);
+        
+        
+        System.out.println(fullName);
+        System.out.println(name);
+        System.out.println(ch);
+    }
+}
+*/
+
+
+/*
+class Hii{
+    public static void main(String[] args){
+        int num = 10;
+        System.out.println(num++);
+        System.out.println(num);
+    }
+}
+*/
+
+// ~ Bitwise Not ( int, short, long, byte ) :  ऑपरेटर हर एक बिट को उलट देता है , 0 को 1 में 1 को 0 में बदल देता है ।
+
+/*
+class Hii{
+    public static void main(String[] args){
+        float num = 1.12345678f; // float लगभग 7 दशमलव अंकों तक सटीक परिणाम देता है।
+        float num1 = 1.1234567f;
+        System.out.println(num);
+        System.out.println(num1);
+    }
+}
+*/
+
+/*
+class Hii{
+    public static void main(String[] args){
+        double num = 1.123456789123456; // 15 decimal places 
+        double num1 = 1.1234567891234567;
+        System.out.println(num);
+        System.out.println(num1);
+    }
+}
+*/
+
+
+/*
+class Hii {
+    public static void main(String[] args) {
+        System.out.println(23+2*3-5); // BODMAS Rule
+        System.out.println(23+2*3-5%7); // // BODMAS Rule
+    }
+}
+*/
+
+/*
+class Hii{
+    public static void main(String[] args){
+        int num1 = 100;
+        int num2 = 100;
+        if(num1 == num2);
+        System.out.println("Equal");   
+        if(num1 > num2);
+        System.out.println("Num 1 is Great");
+        if(num1 < num2);
+        System.out.println("Num 2 is Great");
+        
+    }
+}
+*/
+
+/*
+class Hii{
+    public static void main(String[] args){
+        int num = 0;
+        while(num<=5){
+            System.out.println(num);
+            num++;
         }
-                    if(count!=0)
-            {
-                System.out.println("yes");
-            }
-            else{
-               System.out.println("No"); 
-            }
+    }
+}
+*/
+
+
+class Hii{
+    public static void main(String[] args){
+        int num = 10;
+        while(num<=5){
+            System.out.println(num);
+            num++;
+        }
     }
 }
