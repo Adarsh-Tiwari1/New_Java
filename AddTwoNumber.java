0808b9e7ef74bf1d2ed1327d0c986ba96f63ff8a
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-22 18:37:10 +0530

    Add Two Number

--- AddTwoNumber.java
+++ AddTwoNumber.java
@@ -1,4 +1,6 @@
+/*
 import java.util.Scanner;
+
 public class AddTwoNumber {
     public static void main(String args[]){
         int a,b,sum;
@@ -7,7 +9,25 @@ public class AddTwoNumber {
         a=r.nextInt();
         b=r.nextInt();
         sum = a+b;
-        System.out.println("addition " +(a+b));
+        System.out.println("addition " + a+b);
 
     }
 }
+*/
+
+
+import java.util.Scanner;
+
+public class AddTwoNumber {
+    public static void main(String args[]){
+        int a,b,sum;
+        System.out.println("enter two number");
+        Scanner r=new Scanner(System.in);
+        a=r.nextInt();
+        b=r.nextInt();
+        sum = a+b;
+        System.out.println("addition " + a+b);
+
+    }
+}
+
