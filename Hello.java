a9a8e81d1818a5b52e930009cadfe32885d0274c
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-24 15:15:33 +0530

    Hello

--- Hello.java
+++ Hello.java
@@ -1,7 +1,28 @@
-
+/*
 class Hello{
     public static void main(String[] args){
-        System.out.println("Hello");
-        System.out.println("Adarsh Kumar Tiwari");
+        int String = 10;
+       // char ch = 50;
+      //  char chh = -50; // error
+        char   a = '\n';
+       // System.out.println(String);
+        System.out.println(a);
+    }
+}
+*/
+
+/*
+class Hello {
+    public static void main(String[] args) {
+        char ch = '\uFACE'; // Unicode character
+        System.out.println(ch);
+    }
+}
+*/
+
+class Hello {
+    public static void main(String[] args) {
+       new int[] {10,20};
+        System.out.println(x);
     }
 }
