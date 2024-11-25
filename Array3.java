46e88adfe8d6e44171b13d6b56e8c9cf685d9ffd
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-25 21:09:55 +0530

    Some Array Questio

---
+++ Array3.java
@@ -0,0 +1,33 @@
+// smallest element in the array.
+//. Aarray me index size aur size negative nahi hoti hai
+/*
+class Array3{
+    public static void main(String[] args){
+        int arr[] = {10,20,62,0};
+        int min = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
+*/
+
+// find max value in array 
+/*
+class Array3{
+    public static void main(String[] args){
+        int arr[] = {10,20,62,0,-2,66};
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Largest element : " +  max);
+    }
+}
+*/
+
