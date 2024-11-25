6502b0789e5a2b3613791beee7bb8f4fd3495ecc
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-25 23:54:37 +0530

    Array Question

---
+++ ArrayQue.java
@@ -0,0 +1,64 @@
+/*
+class ArrayQue{
+    public static void main(String[] args){
+        int arr[] = {10,20,-2,30,40,50};
+        int min = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<10){
+                min = arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
+*/
+
+/*
+class ArrayQue{
+    public static void main(String[] args){
+        int arr[] = {20,10,30,50,40};
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
+*/
+
+/*
+class ArrayQue{
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5};
+        int sum = 0;
+        for(int i=0;i<arr.length;i++){
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+    }
+}
+*/
+
+
+import java.util.Scanner;
+class ArrayQue{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Number");
+        int num = sc.nextInt();
+        int arr[] = new int[10];
+        int i = 0;
+        while(num>0){
+            int r = num % 2;
+            arr[i] = r;
+            i++;
+            num = num/2;
+        }
+        System.out.println("Binary Number");
+        for(i=i-1;i>=0;i--){
+            System.out.print(arr[i]+"\t");
+        }
+    }
+}
