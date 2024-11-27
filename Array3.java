9ebf64f102cc3d3c3c326a6164b92ee7af356956
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-27 21:02:17 +0530

    Some Array Question

--- Array3.java
+++ Array3.java
@@ -31,5 +31,55 @@ class Array3{
 }
 */
 
+// Find Seconde Gratest Number
+/*
+import java.util.Scanner;
+
+class Array3{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[7];
+        int FirstMax = arr[0];
+        int SecondMax = arr[0];
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Enter Element : " + i + " :");
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>FirstMax){
+                SecondMax=FirstMax;
+                FirstMax=arr[i];
+            }else if(arr[i]>SecondMax && arr[i]!=FirstMax){
+                SecondMax=arr[i];
+            }
+        }
+        System.out.println("***********************************************************************");
+        System.out.println("First max : " + FirstMax);
+        System.out.println("First max : " + SecondMax);
+    }
+}
+*/
+
+// wap to check given array is sorted or not 
 
 
+
+
+class Array3{
+    public static void main(String[] args){
+        int arr[] = {10,40,20,30};
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    count++;
+                }
+            }
+        } if(count==0){
+            System.out.println("yes array element is sorted");
+        }else{
+            System.out.println("not array element is sorted");
+        }
+    }
+}
