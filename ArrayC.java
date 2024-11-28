51788b0fa44502c6377f4e760401b8e61c76a9cf
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-28 23:33:32 +0530

    1D Array Question

---
+++ ArrayC.java
@@ -0,0 +1,72 @@
+/*
+class ArrayC{
+    public static void main(String[] args){
+        int arr[] = {10,20,30,40,50};
+        arr[0] = 100;
+        System.out.println(arr[0]);
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class ArrayC{
+    public static void main(String[] args){
+        int arr[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 5 value");
+
+        for(int i=0;i<5;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Sorted Value");
+        for(int i=0;i<5;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class ArrayC{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        
+        System.out.println("Enter The Size Of Array");
+        int size = sc.nextInt();
+
+        int num[] = new int[size];
+        for(int i=0;i<size;i++){
+            num[i] = sc.nextInt();
+        }
+        for(int i=0;i<size;i++){
+            sum = sum + num[i];
+        }
+        System.out.println(sum);
+    }
+}
+*/
+
+import java.util.Scanner;
+
+class ArrayC{
+    public static void main(String[] ares){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size Of Array");
+        int size = sc.nextInt();
+        int num[] = new int[Size];
+        for(int i=0;i<size;i++){
+            num[i] = sc.nextInt();
+        }
+        System.out.println("Output");
+        int num[0] = 
+        for(int i=0;i<size;i++){
+            if()
+        }
+    }
+}
