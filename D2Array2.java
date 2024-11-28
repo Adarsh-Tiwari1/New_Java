cbb74f209fdba23db33f0255a100554d447d9801
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-28 23:30:55 +0530

    2D Array

---
+++ D2Array2.java
@@ -0,0 +1,22 @@
+/*
+import java.util.Scanner;
+
+class D2Array2 {
+    public static void main(String[] args) {
+        int arr[][] = new int[2][5];
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.println("Enter Element Index " + i + j + ": ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+*/
