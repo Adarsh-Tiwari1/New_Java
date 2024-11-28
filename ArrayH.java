d87f3f2fbb4f7f17e4fc74f009018a79c5b6059f
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-11-28 13:49:01 +0530

    Some Array Question

---
+++ ArrayH.java
@@ -0,0 +1,38 @@
+// Array Find Max Value
+/*
+class ArrayH{
+    public static void main(String[] args){
+        int arr[] = {10,20,100,30,40,50};
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println("Max Value :" + max);
+    }
+}
+*/
+
+
+/*
+import java.util.Scanner;
+
+class ArrayH {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = sc.nextInt();  
+        int[] num = new int[size]; 
+        for (int i = 0; i < size; i++) {
+            num[i] = sc.nextInt();  
+        }
+        System.out.println("Array elements are:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(num[i] + "\t");  
+        }
+    }
+}
+*/
+
+
