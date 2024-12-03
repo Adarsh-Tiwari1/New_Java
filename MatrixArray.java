455cf82aa7cb3d7f87d5647c691d97a411e3a0dd
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-03 19:58:01 +0530

    Matrix Array

---
+++ MatrixArray.java
@@ -0,0 +1,56 @@
+/*
+import java.util.Scanner;
+
+class MatrixArray{
+    public static void main(String[] ares){
+        int A[][] = new int[3][3];
+        int B[][] = new int[3][3];
+        int C[][] = new int[3][3];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Matrix A ");  // Input Matrix A
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.println("Enter Element Index of " + i+j+" : ");
+                A[i][j]=sc.nextInt();
+            }
+        }
+        System.out.println("Matrix B ");  // Input Matrix B
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.println("Enter Element Index of " + i+j+" : ");
+                B[i][j]=sc.nextInt();
+            }
+        } // Addition Logic
+       for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            C[i][j]=A[i][j]+B[i][j];
+        }
+       }
+       System.out.println("\n Output Matrix A : ");
+       for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            System.out.print("\t" + A[i][j]);
+        }
+        System.out.println("");
+       }// Print Matrix A
+       System.out.println("\n Output Matrix B :");
+       for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            System.out.print("\t" + A[i][j]);
+        }
+        System.out.println("");
+       }// Print Matrix B
+       System.out.println("\n Output Matrix C Addition : ");
+       for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            System.out.print("\t" + C[i][j]);
+        }
+        System.out.println("");
+       }
+    }
+}
+*/
+
+
+
