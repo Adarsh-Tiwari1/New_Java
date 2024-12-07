561e89bf568b3990f746a48a3023b494a1adf892
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-07 19:39:27 +0530

    String method

---
+++ String3.java
@@ -0,0 +1,60 @@
+// indexOf() // Left to Right Working
+/*
+class String3{
+    public static void main(String[] args){
+        String str = "Welcome";
+        int i = str.indexOf("e");
+        int j = str.indexOf("e", i+1);
+        int k = str.indexOf("me");
+        int m = str.indexOf("zee");
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(m);
+    }
+}
+*/
+
+// lastIndexOf() Method // last occurence of the String position
+/*
+class String3{
+    public static void main(String[] args){
+        String str = "Welcome";
+        int i = str.lastIndexOf("e");
+        int j = str.lastIndexOf("e",5);
+        int k = str.lastIndexOf("me");
+        int m = str.lastIndexOf("zee");
+
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(m);
+    }
+}
+*/
+
+// length() Method 
+/*
+class String3{
+    public static void main(String[] args){
+        String str = "Welcome";
+        String str1 = "a b c ";
+        int len = str.length();
+        int len1 = str1.length();
+        System.out.println(len);
+        System.out.println(len1);
+    }
+}
+*/
+
+class String3{
+    public static void main(String[] args){
+        String str = "Welcome";
+        String str1 = "a b c ";
+        int len = str.length();
+        int len1 = str1.length();
+        System.out.println(len);
+        System.out.println(len1);
+    }
+}
