0c3926480f6b3fcd31cf117526d9e37fde6e7eb4
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-09 21:11:54 +0530

    for each loop

---
+++ String4.java
@@ -0,0 +1,24 @@
+/*
+class String4{
+    public static void main(String[] args){
+        String str = "Welcome";
+        char[] arr = {'w','e','l','c','o','m','e'};
+        for(char x:arr){
+            System.out.println(x);
+        } 
+    }
+}
+*/
+
+
+class String4{
+    public static void main(String[] args){
+        
+        int[] num = {10,20,30,40,50};
+        int sum = 0;
+        for(int x:num){
+            sum = sum + x;
+        } 
+        System.out.println(x);
+    }
+}
