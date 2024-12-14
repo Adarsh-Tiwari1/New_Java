0aebc8146e5ee3ba9547e2948162282eb8fff3f8
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-14 21:23:29 +0530

    String Method

---
+++ string.java
@@ -0,0 +1,29 @@
+/*
+class string{
+    public static void main(String[] args){
+        String str = "Adarsh Kumar Tiwari";
+        System.out.println(str.length());
+    }
+}
+*/
+
+/*
+class string{
+    public static void main(String[] args){
+        String str = "Adarsh Kumar Tiwari";
+        for(int i=0;i<str.length();i++){
+            System.out.println(str);
+        }
+    }
+}
+*/
+
+class string{
+    public static void main(String[] args){
+        char[] ch = {'a','d','a','r','s','h'};
+        // for(int i=0;i<ch.length;i++){
+        //     System.out.println(ch);
+        // }
+        System.out.println(ch);
+    }
+}
