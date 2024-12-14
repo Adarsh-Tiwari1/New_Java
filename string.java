f0847695a22718b81b1824db45f49e8849bd6419
Adarsh Tiwari <adarshrewa45@gmail.com>
2024-12-14 23:58:38 +0530

    String Method

--- string.java
+++ string.java
@@ -18,12 +18,105 @@ class string{
 }
 */
 
+/*
 class string{
     public static void main(String[] args){
         char[] ch = {'a','d','a','r','s','h'};
-        // for(int i=0;i<ch.length;i++){
-        //     System.out.println(ch);
-        // }
-        System.out.println(ch);
+        for(int i=0;i<ch.length;i++){
+            System.out.println(ch);
+        }
+        // System.out.println(ch);
     }
+}
+*/
+
+/*
+class string{
+    public static void main(String[] args){
+        char[] ch = {'r','a','m'};
+        String str = new String(ch); // convert char to String
+        System.out.println(str);
+    }
+}
+*/
+
+/*
+class string {
+    public static void main(String[] args) {
+        char[] ch = {'r', 'a', 'm'};
+        // Convert the portion of the char array to a String
+        System.out.println(new String(ch, 1, 2)); // Prints "am"
+    }
+}
+*/
+
+/*
+class string {
+    public static void main(String[] args){
+        char[] ch = {'a','d','a','r','s','h'};
+        String str = new String(ch,1,5);
+        System.out.println(str);
+    }
+}
+*/
+
+/*
+class string{
+    public static void main(String[] args){
+        int[] arr = {66,67,68,69,70};
+        String str = new String(arr,1,3);
+        System.out.println(str);
+    }
+}
+*/
+
+/*
+class string{
+    public static void main(String[] args){
+        String[] str = {"ram","Sita","Radha","Krishna"};
+        // System.out.println(str);
+
+        for(String st : str){  // for each loop
+            System.out.println(st);
+        }
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class string{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String");
+        String str = sc.nextLine();
+
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            System.out.println(ch);
+        }
+    }
+}
+*/
+
+/*
+import java.util.Scanner;
+
+class string{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter string :");
+        String str = sc.next();
+
+        for(int i = str.length()-1;i>=0;i--){
+            char ch = str.charAt(i);
+            System.out.print(ch);
+        }
+    }
+}
+*/
+
+class string{
+    public static void main
 }
