7cba9e2e1f021eb46ea0237570a5b82b9e6ac6f0
Adarsh Tiwari <adarshrewa45@gmail.com>
2025-03-12 21:58:21 +0530

    Array

---
+++ AapnaArray.java
@@ -0,0 +1,52 @@
+// Array Continus memory leta hai, so array ko hum siddha store karte hai.
+// tree siddhi line me store nahi hote hai . 
+/*
+public class AapnaArray {
+    public static void main(String[] args) {
+        char ch = 'A'; 
+        System.out.println("Size of char in Java: " + Character.BYTES + " bytes");
+    }
+}
+*/
+
+/*
+public class AapnaArray{
+    public static void main(String[] args){
+        int i = 2;
+        System.out.println("Size of int in Java: " + Integer.BYTES + " byte");
+    }
+}
+*/
+
+/*
+public class AapnaArray{
+    public static void main(String[] args){
+        double d = 2;
+        System.out.println("Size of int in Java: " + Double.BYTES + " byte");
+    }
+}
+*/
+
+/*
+public class AapnaArray{
+    public static void main(String[] args){
+        Float f = 2.0f;
+        System.out.println("Size of int in Java: " + Float.BYTES + " byte");
+    }
+}
+*/
+
+
+import java.util.Scanner;
+
+class AapnaArray{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = sc.nextInt();
+        int[] arr = new int[20];
+        for(int i=0;i<arr.length;i++){
+
+        }
+    }
+}
+
